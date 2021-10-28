c01e2662e5d3b4cd3d0d98e1cb82756c27253875
Md Ali <mali54@hawk.iit.edu>
2021-10-28 12:50:43 -0500

    Moving Viz files

---
+++ Data-NAS-Benchmarks/sim.scripts.py
@@ -0,0 +1,5 @@
+import sys
+sys.argv = [ "/home/mali54/Sniper-Simulation/sniper/scripts/periodic-stats.py", "1000:2000" ]
+execfile("/home/mali54/Sniper-Simulation/sniper/scripts/periodic-stats.py")
+sys.argv = [ "/home/mali54/Sniper-Simulation/sniper/scripts/markers.py", "markers" ]
+execfile("/home/mali54/Sniper-Simulation/sniper/scripts/markers.py")
